b9d9be3ea3b73433e112f6447b08e19c9e1fe915
loicruterana <lruterana@gmail.com>
2023-08-06 18:03:30 +0200

    test railway

--- kinomatch/src/utils/config.tsx
+++ kinomatch/src/utils/config.tsx
@@ -1,3 +1,3 @@
 const API_BASE_URL = 'https://kinomatch-back-production.up.railway.app';
-// const API_BASE_URL = 'http://localhost:4000';;
+// const API_BASE_URL = 'http://localhost:4000';
 export default API_BASE_URL;
